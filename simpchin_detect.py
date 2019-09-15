0fb269cc06034c2e86bd310c795ff141d5eb43fa
murtazasamiwala <murtazasamiwala@gmail.com>
2019-09-15 21:45:03 +0530

    Some changes to outputs

--- simpchin_detect.py
+++ simpchin_detect.py
@@ -24,7 +24,7 @@ for i in os.listdir(base_path):
     extension=i.split('.')[-1]
     if extension==i:
         pass
-    elif extension in ['py','git','spec','exe','txt']:
+    elif extension in ['py','git','spec','exe','txt','md','gitattributes']:
         pass
     elif extension=='docx':
         text_doc=docx2txt.process(i)
@@ -33,11 +33,11 @@ for i in os.listdir(base_path):
             msg='{} does not contain Chinese text'.format(i)
             msg_list.append(report(msg,i,'IGNORE FILE'))
         elif chinese_text.issubset(trad):
-            msg='{} is written in Traditional Chinese'.format(i)
-            msg_list.append(report(msg,i,'PASSED'))
+            msg='{} is written in Traditional Chinese.\nConfirm that the service is E to TC. Otherwise, it is a serious error.'.format(i)
+            msg_list.append(report(msg,i,'TRADITIONAL CHINESE'))
         elif chinese_text.issubset(simp):
-            msg='{} is written in Simplified Chinese'.format(i)
-            msg_list.append(report(msg,i,'ERROR'))
+            msg='{} is written in Simplified Chinese.\nConfirm that the service is E to SC. Otherwise, it is a serious error.'.format(i)
+            msg_list.append(report(msg,i,'SIMPLIFIED CHINESE'))
         else:
             output=open('output_'+i.split('.')[0]+'.txt','a',encoding='utf8')
             for char in chinese_text:
@@ -45,8 +45,8 @@ for i in os.listdir(base_path):
                     output.write(char+'\n')
             output.close()
             output_name='output_'+i.split('.')[0]+'.txt'
-            msg_strt='{} has both Simplified and Traditional characters'.format(i)+'\n'
-            msg_end=' has been generated. It is a list of Simplified Characters to be fixed'
+            msg_strt='{} has both Simplified and Traditional characters'.format(i)+'\n'+'Check service name and fix characters of other language.'+'\n'
+            msg_end=' has been generated. It is a list of Simplified Characters to be fixed.'
             full_msg=msg_strt+output_name+msg_end
             msg_list.append(report(full_msg,i,'ERROR'))
     else:
